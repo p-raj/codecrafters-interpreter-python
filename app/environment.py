ff10cd8b4ab2900a0c3892aacc4313abe0caaa93
Peeyush Raj <praj@veris.in>
2026-05-30 20:16:16 +0530

    codecrafters submit [skip ci]

--- app/environment.py
+++ app/environment.py
@@ -12,18 +12,20 @@ class Environment:
         self.values[name] = value
 
     def get(self, name: Token) -> object:
-        try:
+        if name.lexeme in self.values:
             return self.values[name.lexeme]
-            if self.enclosing:
-                return self.enclosing.get(name)
-        except KeyError:
-            raise InterpreterException(
-                name, "Undefined variable '" + name.lexeme + "'."
-            )
-
-    def assign(self, name: Token, value: object):
         if self.enclosing:
-            self.enclosing.assign(name, object)
-        else:
-            self.get(name)
-            self.define(name.lexeme, value)
+            return self.enclosing.get(name)
+
+        raise InterpreterException(name, "Undefined variable '" + name.lexeme + "'.")
+
+    def assign(self, name: Token, value: object) -> None:
+        if name.lexeme in self.values:
+            self.values[name.lexeme] = value
+            return
+
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
+            return
+
+        raise InterpreterException(name, f"Undefined variable '{name.lexeme}'.")
